9a5d7bae7c22474d126bfdda7cbb5d705a2f85ac
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-01-19 16:55:06 +0000

    work

--- high_scores/src/high_scores.py
+++ high_scores/src/high_scores.py
@@ -1,10 +1,14 @@
-def latest(scores):
-    latest_score = [scores]
-
+def latest(score_list):
+    return score_list[-1]
 
 def personal_best(scores):
-    pass
-
+    return max(scores)
 
 def personal_top_three(scores):
-    pass
+    scores.sort()
+    return [scores[-1], scores[-2], scores[-3]]
+
+def high_to_low_scores(scores):
+    scores.sort(reverse=True)
+    return scores
+
